66ce6208824ea4f8acd5f3324fbbbb74dd1611ae
hyunsu <rkdgustn77@naver.com>
2024-11-25 23:08:12 +0900

    commit

--- src/코딩테스트/프로그래머스/level1/달리기경주_2.kt
+++ src/코딩테스트/프로그래머스/level1/달리기경주_2.kt
@@ -1,9 +1,11 @@
 package 코딩테스트.프로그래머스.level1
 
+//https://school.programmers.co.kr/learn/courses/30/lessons/178871
 fun main() {
     val players = arrayOf("mumu", "soe", "poe", "kai", "mine")
-    val callings = arrayOf("kai", "kai", "mine", "mine")
+    val callings = arrayOf("kai", "kai", "kai", "poe", "poe", "poe")
 
+    //시간초과
     class Solution {
         fun solution(players: Array<String>, callings: Array<String>): Array<String> {
             var answer: Array<String> = arrayOf<String>()
@@ -16,8 +18,9 @@ fun main() {
 
             callings.forEach {
                 val callingRanking = playerRanking[it]!!
-                playerRanking[it] = callingRanking.minus(1)
+                //find를 통해 처음부터 끝까지 순위가 맞는사람을 찾기 때문에 타임아웃 에러가 발생
                 val callingFrontName = playerRanking.entries.find { it.value == callingRanking.minus(1) }!!.key
+                playerRanking[it] = callingRanking.minus(1)
                 playerRanking[callingFrontName] = playerRanking[callingFrontName]!!.plus(1)
             }
 
